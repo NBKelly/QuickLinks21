be934e8870fce9d4f965fc5a9dff73027287ccac
NB Kelly <N.B.Kelly@protonmail.com>
2021-01-23 17:28:48 +1300

    output format looks roughly like the last one (2019 quicklinks), and also takes the sans-parallel data

--- time.sh
+++ time.sh
@@ -19,15 +19,62 @@ do
     java com.nbkelly.QuickLinks21 --file "$entry" > ans_tmp.txt
     java com.nbkelly.QuickLinks21 --file "$entry" > ans_tmp.txt
     java com.nbkelly.QuickLinks21 --file "$entry" > ans_tmp.txt
+    
+    java com.nbkelly.QuickLinks21 --file "$entry" > ans_tmp.txt
+    java com.nbkelly.QuickLinks21 --file "$entry" > ans_tmp.txt
+    java com.nbkelly.QuickLinks21 --file "$entry" > ans_tmp.txt
+    java com.nbkelly.QuickLinks21 --file "$entry" > ans_tmp.txt
+    java com.nbkelly.QuickLinks21 --file "$entry" > ans_tmp.txt
     end=`date +%s.%N`
     
-    runtime=$(python3 -c "print((${end} - ${start})/5.0)")    
-    echo "Total: " $runtime
+    runtime=$(python3 -c "print((${end} - ${start})/10.0)")        
 
+    start=`date +%s.%N`
+    java com.nbkelly.QuickLinks21 --only-preprocess --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --only-preprocess --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --only-preprocess --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --only-preprocess --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --only-preprocess --file "$entry" > ans_trash.txt
+
+    java com.nbkelly.QuickLinks21 --only-preprocess --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --only-preprocess --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --only-preprocess --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --only-preprocess --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --only-preprocess --file "$entry" > ans_trash.txt
+    end=`date +%s.%N`
+    runtime2=$(python -c "print((${end} - ${start})/10.0)")
+
+    start=`date +%s.%N`
+    java com.nbkelly.QuickLinks21 --disable-parallel --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --disable-parallel --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --disable-parallel --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --disable-parallel --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --disable-parallel --file "$entry" > ans_trash.txt
+
+    java com.nbkelly.QuickLinks21 --disable-parallel --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --disable-parallel --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --disable-parallel --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --disable-parallel --file "$entry" > ans_trash.txt
+    java com.nbkelly.QuickLinks21 --disable-parallel --file "$entry" > ans_trash.txt
+    end=`date +%s.%N`
+    
+    runtime4=$(python -c "print((${end} - ${start})/10.0)")
+
+
+    runtime3=$(python -c "print((${runtime} - ${runtime2}))")
+    #display the difference in runtimes
+
+    echo "Total:         " $runtime
+    echo "Sans-Parallel: " $runtime4
+    echo "Pre:           " $runtime2
+    echo "Post:          " $runtime3    
+    
     #now we want to see if the answer is valid 
     DIFF=$(diff "ans_tmp.txt" "$ansName") > differences
     wc -l differences
     rm differences
+    rm ans_tmp.txt
+    rm ans_trash.txt
 
     echo
 done
